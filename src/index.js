d8b156c1d4c803c627e52aa030d609cb3ea69d4e
PamelaBarboza <pamelarabarboza@gmail.com>
2020-06-11 20:44:17 -0400

    cambios en html, creacion de secciones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import cipher from './cipher.js';
-
-console.log(cipher);
+window.onload=function(){
+    let btn = document.getElementById('btn');
+    btn.addEventListener('click', () => {
+    let input = document.getElementById("input");
+    let valor = input.value;
+    document.getElementById("fname").innerHTML = "¡Hola " + valor + "!";
+    });
+    }
