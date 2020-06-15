bb54b40294d82f9242e35bbc8933757bc9861238
PamelaBarboza <pamelarabarboza@gmail.com>
2020-06-15 19:57:32 -0400

    funciona el caesar

--- src/index.js
+++ src/index.js
@@ -24,47 +24,79 @@ comenzar.addEventListener("click", function () {
 
   });
    //QUINTA PANTALLA
-  generar.addEventListener("click", function () {
-    generador.style.display = "none";
-    final.style.display = "block";
+   let generar = document.getElementById('generar');
+    generar.addEventListener("click", function () {
+      let input1 = document.getElementById("input1");
+      let valor = input1.value;
 
-  });
-
-  
-   //SEXTA PANTALLA
-
-  inicio1.addEventListener("click", function () {
-    final.style.display = "none";
-    contenedor.style.display = "block";
-
-    const caesarCipher = (string, n)=>{
+      const caesarCipher = (string, n)=>{    
         let result = "", code;
-        let input = document.getElementById("input1");
-    let valor = input.value;
         for (let i=0; i<string.length;i++){
           if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
             code = (((string.charCodeAt(i)-65)+n)%26)+65;
           }
-    
+          else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+            code = (((string.charCodeAt(i)-97)+n)%26)+97;
+          }
           else if (string.charCodeAt(i)===32){
             code = 32;
           }
           result+=String.fromCharCode(code);
         }
         return result;
-      }
+              
+  }
+
+      document.getElementById("tuClave").innerHTML = "TU NUEVA CLAVE ES " +  caesarCipher(input1.value, 3);
+
+    /*generador.style.display = "none";
+    final.style.display = "block";*/
+
+    });
+
+   
+ 
+  
+   //SEXTA PANTALLA
+  inicio1.addEventListener("click", function () {
+    final.style.display = "none";
+    contenedor.style.display = "block";
+
     
-      document.getElementById(caesarCipher("tuClave", 3)).innerHTML =  valor;
-      
 
-  });
-  /*let btn = document.getElementById('btn');
-  btn.addEventListener('click', () => {
-  let input = document.getElementById("input");
-  let valor = input.value;
-  document.getElementById("fname").innerHTML = "¡Hola " + valor + "!";
-  }); */
+    /*window.onload=function(){
+    let btn = document.getElementById('btn');
+    btn.addEventListener('click', () => {
+    let input = document.getElementById("input");
+    let valor = input.value;
+    document.getElementById("fname").innerHTML = "¡Hola " + valor + "!";
+    });
+    } 
+*/
+});
 
+    const caesarCipher = (string, n)=>{    
+        let result = "", code;
+        for (let i=0; i<string.length;i++){
+          if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+            code = (((string.charCodeAt(i)-65)+n)%26)+65;
+          }
+          else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+            code = (((string.charCodeAt(i)-97)+n)%26)+97;
+          }
+          else if (string.charCodeAt(i)===32){
+            code = 32;
+          }
+          result+=String.fromCharCode(code);
+        }
+        return result;
+              
+  }
+   /*console.log(caesarCipher(input1.value, 3));*/
+   
+  
+
+ 
 
    //SEPTIMA PANTALLA
   crear1.addEventListener("click", function () {
