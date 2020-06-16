6f577a6a9417bd85f2fe96c2476bf24937edcc5e
PamelaBarboza <pamelarabarboza@gmail.com>
2020-06-16 10:21:25 -0400

    Detalles en el cifrado

--- src/index.js
+++ src/index.js
@@ -24,30 +24,31 @@ comenzar.addEventListener("click", function () {
 
   });
    //QUINTA PANTALLA
-   let generar = document.getElementById('generar');
-    generar.addEventListener("click", function () {
-      let input1 = document.getElementById("input1");
-      let valor = input1.value;
+   let generar = document.getElementById('generar'); //tomo al elemento por el Id generar, y lo guardo en una variable 
+    generar.addEventListener("click", function () { //le asigno una funcion click y cuando el evento se ejecute en generar ocurre: 
+      let input1 = document.getElementById("input1");//tomo al elemento por el Id input1, y lo guardo en una variable 
+      let valor = input1.value; //luego almanceno en valor el value de input1
+      let menuOpcion = document.getElementById("menuOpcion");//tomo al elemento por el Id menuOpcion, y lo guardo en una variable 
+      let valor2 = menuOpcion.value;//luego almanceno en valor el value de menuOpcion
 
-      const caesarCipher = (string, n)=>{    
-        let result = "", code;
-        for (let i=0; i<string.length;i++){
-          if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-            code = (((string.charCodeAt(i)-65)+n)%26)+65;
+      const caesarCipher = (string, n)=>{ //se declara una constante de tipo string que tiene dos parametros
+          let result = "", code;
+          for (let i=0; i<string.length;i++){ //el ciclo for analiza letra por letra va entrando en cada if segun corresponda
+            if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+              code = (((string.charCodeAt(i)-65)+n)%26)+65;
+            }
+            else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+              code = (((string.charCodeAt(i)-97)+n)%26)+97;
+            }
+            else if (string.charCodeAt(i)===32){
+              code = 32;
+            }
+            result+=String.fromCharCode(code);
           }
-          else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
-            code = (((string.charCodeAt(i)-97)+n)%26)+97;
-          }
-          else if (string.charCodeAt(i)===32){
-            code = 32;
-          }
-          result+=String.fromCharCode(code);
+          return result;
         }
-        return result;
-              
-  }
 
-      document.getElementById("tuClave").innerHTML = "TU NUEVA CLAVE ES " +  caesarCipher(input1.value, 3);
+      document.getElementById("tuClave").innerHTML = "TU NUEVA CLAVE ES: " +  caesarCipher(valor, valor2); //Toma el elemento id tuClave e imprime en pantalla.
 
     /*generador.style.display = "none";
     final.style.display = "block";*/
