c8c2b336afe1965069dc88040c75682db8e948ff
PamelaBarboza <pamelarabarboza@gmail.com>
2020-06-16 10:56:20 -0400

    ANDUVO EL CIFRADO

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ comenzar.addEventListener("click", function () {
       let input1 = document.getElementById("input1");//tomo al elemento por el Id input1, y lo guardo en una variable 
       let valor = input1.value; //luego almanceno en valor el value de input1
       let menuOpcion = document.getElementById("menuOpcion");//tomo al elemento por el Id menuOpcion, y lo guardo en una variable 
-      let valor2 = menuOpcion.value;//luego almanceno en valor el value de menuOpcion
+      let valor2 = parseInt(menuOpcion.value);//luego almanceno en valor el value de menuOpcion
 
       const caesarCipher = (string, n)=>{ //se declara una constante de tipo string que tiene dos parametros
           let result = "", code;
@@ -40,7 +40,7 @@ comenzar.addEventListener("click", function () {
             else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
               code = (((string.charCodeAt(i)-97)+n)%26)+97;
             }
-            else if (string.charCodeAt(i)===32){
+            else if (string.charCodeAt(i)===32){ //Analiza si el numero es considerado como 32 lo Toma como un espacio
               code = 32;
             }
             result+=String.fromCharCode(code);
