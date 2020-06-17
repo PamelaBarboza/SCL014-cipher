58fb442475bded4ed8a7276e68a38bdfdba5488d
PamelaBarboza <pamelarabarboza@gmail.com>
2020-06-17 15:05:11 -0400

    import & export

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
+
    //PRIMERA PANTALLA
     window.onload=function(){
     comenzar.addEventListener("click", function () {
@@ -21,13 +22,13 @@ console.log(cipher);
     principal.style.display ="none";
     cuerpo.style.display="block";
 
-  })
+  });
 
     //TERCERA PANTALLA (descifrar- septima pantalla HTML)
     descifrar.addEventListener("click", function(){
     principal.style.display ="none";
     decypher.style.display ="block";
-  })
+  });
 
    //CUARTA PANTALLA (cifrar)
     siguiente.addEventListener("click", function () {
@@ -42,15 +43,15 @@ console.log(cipher);
     generador.style.display = "block";
 
   });
-   /*
+  
       let generar = document.getElementById('generar'); //tomo al elemento por el Id generar, y lo guardo en una variable 
       generar.addEventListener("click", function () { //le asigno una funcion click y cuando el evento se ejecute en generar ocurre: 
       let input1 = document.getElementById("input1");//tomo al elemento por el Id input1, y lo guardo en una variable 
       let valor = input1.value; //luego almanceno en valor el value de input1
       let menuOpcion = document.getElementById("menuOpcion");//tomo al elemento por el Id menuOpcion, y lo guardo en una variable 
       let valor2 = parseInt(menuOpcion.value);//luego almanceno en valor el value de menuOpcion
-
-      const caesarCipher = (string, n)=>{ //se declara una constante de tipo string que tiene dos parametros
+       /*
+      const cipher = (string, n)=>{ //se declara una constante de tipo string que tiene dos parametros
           let result = "", code;
           for (let i=0; i<string.length;i++){ //el ciclo for analiza letra por letra va entrando en cada if segun corresponda
             if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
@@ -66,11 +67,11 @@ console.log(cipher);
           }
           return result;
         }
-
-      document.getElementById("tuClave").innerHTML = "TU NUEVA CLAVE ES: " +  caesarCipher(valor, valor2); //Toma el elemento id tuClave e imprime en pantalla.
-      
-  });
+             
 */
+    document.getElementById("tuClave").innerHTML = "TU NUEVA CLAVE ES: " +  cipher(valor, valor2); //Toma el elemento id tuClave e imprime en pantalla.
+  });
+  
    //QUINTA PANTALLA (opciones crear e inicio)
     crear1.addEventListener("click", function () {
     generador.style.display = "none";
@@ -89,15 +90,16 @@ console.log(cipher);
     contenedor.style.display = "block";
   
   });
-/*
+
+  
     let generar2 = document.getElementById('generar2'); 
     generar2.addEventListener("click", function () {  
     let input2 = document.getElementById("input2");
     let valor3 = input2.value;
     let menuOpcion2 = document.getElementById("menuOpcion2");
     let valor4 = parseInt(menuOpcion2.value);
-
-    const caesarCipher2 = (string, n)=>{
+/*
+    const decipher = (string, n)=>{
       let result = "", code;
           for (let i=0; i<string.length;i++){ 
             if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
@@ -113,11 +115,13 @@ console.log(cipher);
           }
           return result;
     }
-
-    document.getElementById("tuClave2").innerHTML = "TU CLAVE DESCRIFRADA ES: " +  caesarCipher2 (valor3, valor4); //Toma el elemento id tuClave e imprime en pantalla.
+*/
+    document.getElementById("tuClave2").innerHTML = "TU CLAVE DESCRIFRADA ES: " +  decipher (valor3, valor4); //Toma el elemento id tuClave e imprime en pantalla.
 
 
   });
-  */
-  }
-
+  
+}
+  
+  
+ 
