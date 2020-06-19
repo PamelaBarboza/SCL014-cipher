f85ccd5bb9ba1b8e5053787991fd273403f52cad
CatalinaJL <catalina.p.jl@gmail.com>
2020-06-19 19:01:32 -0400

    pruebas nuevas

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -34,3 +34,38 @@ describe('cipher', () => {
   });
 
 });
+
+describe('cipher', () => {
+
+  test('should be an object', () => {
+    expect(typeof cipher).toBe('object');
+  });
+
+  describe('cipher.encode', () => {
+
+    test('should be a function', () => {
+      expect(typeof cipher.encode).toBe('function');
+    });
+    test('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
+      //completa este test!
+        expect(cipher.encode("CATA", 33)).toBe("SQJQ");
+
+    });
+
+    
+  });
+
+  describe('cipher.decode', () => {
+
+    test('should be a function', () => {
+      expect(typeof cipher.decode).toBe('function');
+    });
+
+    test('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset -16', () => {
+      //completa este test!
+      expect(cipher.decode("SQJS", -16)).toBe("CA:A");
+    });
+
+  });
+
+});
